f13d600b629e3d73ed27b614ad713ea8c3bc0c1f
ge1kin <aezzeaaa@gmail.com>
2025-11-02 22:54:32 +0200

    updated the inputs to have ":"

--- main.py
+++ main.py
@@ -42,21 +42,22 @@ def add_song(song_name: str, artist: str, genre: str, duration: tuple) -> None:
 
 
 def main():
-    song_amount = int(input("Enter the amount of songs you would like to add as an integer number"))
+    song_amount = int(input("Enter the amount of songs you would like to add as an integer number: "))
     i = 0
     while i < song_amount:
-        song_name = input("Enter the name of the song")
+        song_name = input("Enter the name of the song: ")
         while song_name in liked_songs.keys():
             lower_amount = input("That song is already in the playlist of liked songs. would you like to lower the amount of songs you would like to add? ")
             if lower_amount.lower() == "yes":
                 i += 1
             i += 1
-            song_name = input("Enter the name of the song")
-        artist = input("Enter the name of the artist")
-        genre = input("Enter the genre")
-        duration = (int(input("Enter the amount of minuets")), int(input("Enter the amount of secondes")))
+            song_name = input("Enter the name of the song: ")
+        artist = input("Enter the name of the artist: ")
+        genre = input("Enter the genre: ")
+        duration = (int(input("Enter the amount of minuets: ")), int(input("Enter the amount of secondes: ")))
         add_song(song_name, artist, genre, duration)
 
 
 if __name__ == "__main__":
     main()
+
